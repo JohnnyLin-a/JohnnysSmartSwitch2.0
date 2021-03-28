dde467ef7d931534e0a6621dd31a65c72fdd5dad
JohnnyLin-a <57846020+JohnnyLin-a@users.noreply.github.com>
2021-03-28 15:13:09 -0400

    Remove unused config params

--- config/index.js
+++ config/index.js
@@ -4,14 +4,5 @@ const envFound = dotenv.config();
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 module.exports = {
-    api_key: process.env.API_KEY,
     port: process.env.PORT,
-
-    postgres_config: {
-        user: process.env.POSTGRES_USER,
-        host: process.env.POSTGRES_HOST,
-        database: process.env.POSTGRES_DB,
-        password: process.env.POSTGRES_PASSWORD,
-        port: process.env.POSTGRES_PORT,
-    }
 }
