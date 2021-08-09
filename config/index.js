7d6422d35f942afa38149de3b78b49d335408ad7
JohnnyLin-a <57846020+JohnnyLin-a@users.noreply.github.com>
2021-08-08 22:54:14 -0400

    Remove unnecessary file

--- config/index.js
+++
@@ -1,5 +0,0 @@
-export const port = process.env.PORT;
-export const wolMacAddress = process.env.WOL_MAC_ADDR;
-export const broadcastAddress = process.env.BROADCAST_ADDR;
-export const kasaDeviceIP = process.env.KASA_DEVICE_IP;
-export const magicHomeDeviceIP = process.env.MAGIC_HOME_DEVICE_IP;
