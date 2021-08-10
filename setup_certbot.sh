0cbd15a5da72a69ac4c872c64e97c65f9666fe6d
JohnnyLin-a <57846020+JohnnyLin-a@users.noreply.github.com>
2021-08-09 23:22:41 -0400

    Add steps to copy certs to certs folder

--- setup_certbot.sh
+++ setup_certbot.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+docker pull alpine:latest
 docker run -it --rm -v "/etc/letsencrypt:/etc/letsencrypt" -v "/var/lib/letsencrypt:/var/lib/letsencrypt" -p 80:80 -p 443:443 certbot/certbot:$(cat .certbot_tag) certonly
 docker run -it --rm -v "/etc/letsencrypt:/etc/letsencrypt" -v "/var/lib/letsencrypt:/var/lib/letsencrypt" -p 80:80 -p 443:443 certbot/certbot:$(cat .certbot_tag) renew --dry-run
+mkdir -p nginx/certs
+docker run --rm -w=/root/app -v $(pwd):/root/app -v /etc/letsencrypt:/root/certs alpine:latest /bin/sh -c "cp /root/certs/live/$(cat .domain_name)/*.pem /root/app/nginx/certs/"
